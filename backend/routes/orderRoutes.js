4dabdced17c922ad16493212fe71a81aa946d4ca
yxgdev <yxg.devs@gmail.com>
2022-04-20 00:46:37 +0800

    added order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const { getOrders, checkOut } = require("../controllers/orderController");
+
+const router = Router();
+
+router.get("/order/:id", getOrders);
+router.post("/order:id", checkOut);
+
+module.exports = router;
