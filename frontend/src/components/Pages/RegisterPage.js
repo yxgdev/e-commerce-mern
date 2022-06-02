2a29f26e87cb89d700066da771aa690254f7c2bb
yxgdev <yxg.devs@gmail.com>
2022-06-02 13:37:45 +0800

    modified register page

--- frontend/src/components/Pages/RegisterPage.js
+++ frontend/src/components/Pages/RegisterPage.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import AuthNavbar from "../Auth/AuthNavbar/AuthNavbar";
 import RegisterSection from "../Auth/RegisterSection/RegisterSection";
 import Footer from "../Categories/Footer/Footer";
 
 const RegisterPage = () => {
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    auth.user && navigate("/");
+  }, [auth]);
   return (
     <div>
       <AuthNavbar />
